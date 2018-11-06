9cc13e20678ed2008e6f7796e3406c085d0b0afe
Erik Fleckstein <erik@tinkerforge.com>
2018-11-06 15:54:27 +0100

    Add rust examples

---
+++ software/examples/rust/example_simple.rs
@@ -0,0 +1,29 @@
+use std::{error::Error, io};
+
+use tinkerforge::{ipconnection::IpConnection, isolator_bricklet::*};
+
+const HOST: &str = "127.0.0.1";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Isolator Bricklet
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection
+    let isolator_bricklet = IsolatorBricklet::new(UID, &ipcon); // Create device object
+
+    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
+                                        // Don't use device before ipcon is connected
+
+    // Get current statistics
+    let get_statistics_result = isolator_bricklet.get_statistics().recv()?;
+
+    println!("Messages From Brick: {}", get_statistics_result.messages_from_brick);
+    println!("Messages From Bricklet: {}", get_statistics_result.messages_from_bricklet);
+    println!("Connected Bricklet Device Identifier: {}", get_statistics_result.connected_bricklet_device_identifier);
+    println!("Connected Bricklet UID: {}", get_statistics_result.connected_bricklet_uid);
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}
