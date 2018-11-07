f6edac2fd40e6dc0e81fcdd0a223e17523b0c36c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:23:16 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, isolator_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, isolator_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Isolator Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Isolator Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let isolator_bricklet = IsolatorBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let i = IsolatorBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current statistics
-    let get_statistics_result = isolator_bricklet.get_statistics().recv()?;
+    // Get current statistics.
+    let get_statistics_result = i.get_statistics().recv()?;
 
     println!("Messages From Brick: {}", get_statistics_result.messages_from_brick);
     println!("Messages From Bricklet: {}", get_statistics_result.messages_from_bricklet);
