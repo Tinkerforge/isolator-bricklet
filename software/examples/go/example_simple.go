ed012c84731ab96f508a095a9e209a1cb4661cda
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:11:30 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/isolator_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Isolator Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	i, _ := isolator_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get current statistics.
+	messagesFromBrick, messagesFromBricklet, connectedBrickletDeviceIdentifier, connectedBrickletUID, _ := i.GetStatistics()
+
+	fmt.Printf("Messages From Brick: \n", messagesFromBrick)
+	fmt.Printf("Messages From Bricklet: \n", messagesFromBricklet)
+	fmt.Printf("Connected Bricklet Device Identifier: \n", connectedBrickletDeviceIdentifier)
+	fmt.Printf("Connected Bricklet UID: \n", connectedBrickletUID)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
