dc9052c330cc568a0fd14e33d3d30bc8bd67e19f
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:03:43 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,13 +9,13 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_Isolator i;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_isolator_create(&i, UID, hal), "create device object");
 
@@ -32,7 +32,7 @@ void example_setup(TF_HalContext *hal) {
 	tf_hal_printf("Connected Bricklet UID: %s\n", connected_bricklet_uid);
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
