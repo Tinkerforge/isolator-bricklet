7e575810398f31fb39a3590556a5dcfed06f85cc
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:40:38 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_isolator.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_isolator.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Isolator Bricklet
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_Isolator i;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_isolator_create(&i, UID, hal), "create device object");
+	check(tf_isolator_create(&i, NULL, hal), "create device object");
 
 	// Get current statistics
 	uint32_t messages_from_brick, messages_from_bricklet;
